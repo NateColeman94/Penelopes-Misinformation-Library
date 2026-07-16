2c5f11554a4049bf6e384f940e5837100ae64068
NateColeman94 <nathan.coleman2012@gmail.com>
2026-07-16 14:19:12 -0400

    Add files via upload

---
+++ data/desk-data.js
@@ -0,0 +1,44 @@
+window.PENELOPE_DESK_DATA = {
+  "currentlyReading": [
+    "the hobbit",
+    "pride and prejudice",
+    "coraline",
+    "one piece",
+    "matilda"
+  ],
+  "recommendations": [
+    "charlottes web",
+    "six of crows",
+    "the great gatsby",
+    "the screwtape letters",
+    "the hunger games"
+  ],
+  "misunderstandings": [
+    "moby dick",
+    "green eggs and ham",
+    "the fellowship of the ring",
+    "animal farm",
+    "twilight"
+  ],
+  "notices": [
+    "Please stop shelving dragons under Pets.",
+    "Someone returned a cookbook full of glitter.",
+    "Lost: one respectable summary.",
+    "The mystery section refuses to stay alphabetized.",
+    "Goose feathers are not approved bookmarks."
+  ],
+  "mail": [
+    {
+      "from": "Gandalf",
+      "text": "I am not a wizard with poor time management."
+    },
+    {
+      "from": "The Dragons",
+      "text": "We have unionized and request better shelf space."
+    },
+    {
+      "from": "Interlibrary Loan",
+      "text": "Kevin remains under administrative review."
+    }
+  ]
+};
